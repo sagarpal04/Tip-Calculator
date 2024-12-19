8c0c910ea118f6c20680c5e7bd09e62e13351d15
sagarpal04 <sagarpal0402@gmail.com>
2024-12-19 20:11:48 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  function handleReset() {
+    setData({
+      bill: "",
+      yourTip: 0,
+      friendTip: 0,
+    });
+  }
+  function calcTip() {
+    const totalTip =
+      (data.bill * data.yourTip) / 100 + (data.bill * data.friendTip) / 100;
+    return parseFloat((totalTip / 2).toFixed(2));
+  }
 
+  const [data, setData] = useState({
+    bill: "",
+    yourTip: 0,
+    friendTip: 0,
+  });
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="p-2 font-semibold flex flex-col gap-4 items-start">
+      <div className="flex flex-col gap-3">
+        <div className="flex gap-2 items-center">
+          <p>How much was the bill? </p>
+          <input
+            value={data.bill}
+            onChange={(e) => setData({ ...data, bill: e.target.value })}
+            type="text"
+            placeholder="Enter your bill"
+            className="border-2 border-black rounded-lg px-1"
+          />
+        </div>
+        <div className="flex gap-2 items-center">
+          <p>How much was the bill? </p>
+          <select
+            value={data.yourTip}
+            onChange={(e) => setData({ ...data, yourTip: e.target.value })}
+            className="border-2 border-black rounded-lg"
+          >
+            <option value={0}>Dissatisfied (0%)</option>
+            <option value={5}>It was okay (5%)</option>
+            <option value={10}>It was good (10%)</option>
+            <option value={20}>Absolutely (20%)</option>
+          </select>
+        </div>
+        <div className="flex gap-2 items-center">
+          <p>How did your friend like the service? </p>
+          <select
+            value={data.friendTip}
+            onChange={(e) => setData({ ...data, friendTip: e.target.value })}
+            className="border-2 border-black rounded-lg"
+          >
+            <option value={0}>Dissatisfied (0%)</option>
+            <option value={5}>It was okay (5%)</option>
+            <option value={10}>It was good (10%)</option>
+            <option value={20}>Absolutely (20%)</option>
+          </select>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      {data.bill && (
+        <h2 className="text-2xl font-bold">
+          You pay ${Number(data.bill) + calcTip()} (${data.bill} + ${calcTip()}{" "}
+          tip)
+        </h2>
+      )}
+      <button
+        onClick={() => handleReset()}
+        className="px-2 py-1 border-2 border-black rounded-lg bg-gray-200"
+      >
+        Reset
+      </button>
+    </div>
+  );
+};
 
-export default App
+export default App;
